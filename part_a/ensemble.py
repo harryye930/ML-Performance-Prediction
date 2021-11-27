489405b0772a4ef0f1cf3ebbf5899ec619b20ca4
harry <harry.ye@mail.utoronto.ca>
2021-11-27 12:02:24 -0500

    finish ensemble.py

--- part_a/ensemble.py
+++ part_a/ensemble.py
@@ -1,7 +1,6 @@
 import os.path
 
-import sklearn
-import numpy
+import numpy as np
 from sklearn.impute import KNNImputer
 from torch.autograd import Variable
 import torch
@@ -9,7 +8,8 @@ import torch.optim as optim
 import pandas as pd
 from utils import *
 from neural_network import AutoEncoder
-
+import random
+from neural_network import evaluate
 
 
 def load_data(base_path="../data"):
@@ -33,55 +33,44 @@ def load_data(base_path="../data"):
     return train_matrix, valid_data, test_data
 
 
-def bagging():
-    num_std = 542
-    num_q = 1774
+def bagging(train_matrix):
 
-    train_data = pd.read_csv("train_data.csv")
-    num_std_bagged = int(len(train_data))
-    train_data_sampled = train_data.sample(num_std_bagged, replace=True)
-    train_matrix = np.empty((num_std, num_q))
-    train_matrix[:] = np.NaN
-    data = {"user_id": [],
-            "question_id": [],
-            "is_correct": []}
-    for index in train_data_sampled.index:
-        col = train_data["question_id"][index]
-        row = train_data["user_id"][index]
-        train_matrix[row][col] = train_data["is_correct"][index]
-        data["question_id"].append(train_data["question_id"][index])
-        data["user_id"].append(train_data["user_id"][index])
-        data["is_correct"].append(train_data["is_correct"][index])
+    N, K = train_matrix.shape
+    num_std_loss = int(N * 1/3)
+    random_indeies = random.sample(range(0, N), num_std_loss)
+    train_matrix = train_matrix.copy()
 
-    return data, train_matrix
+    for row in random_indeies:
+        empty_array = np.full((1, K), np.NaN)
+        train_matrix[row] = empty_array
+
+    return train_matrix
 
 
-def eval_knn_base_models(train_data_bagged, train_matrix_bagged, valid_data):
-    nbrs = KNNImputer(n_neighbors=11)  # best performing k = 11
+def eval_knn_base_models(k, train_matrix_bagged, valid_data):
+    nbrs = KNNImputer(n_neighbors=k)  # best performing k = 11
     knn_result_matrix = nbrs.fit_transform(train_matrix_bagged)
     knn_results = sparse_matrix_predictions(valid_data, knn_result_matrix, threshold=0.5)
     return knn_results
 
 
-def eval_neural_net_base_model(train_data_bagged, train_matrix_bagged, valid_data):
+def eval_neural_net_base_model(train_matrix_bagged, valid_data, epoch):
     zero_train_matrix = train_matrix_bagged.copy()
-    zero_train_matrix[np.isnan(train_matrix)] = 0
+    zero_train_matrix[np.isnan(train_matrix_bagged)] = 0
     train_matrix_bagged = torch.FloatTensor(train_matrix_bagged)
     zero_train_matrix = torch.FloatTensor(zero_train_matrix)
 
-    np.random.seed(0)
-    torch.manual_seed(0)
     k = 10
     num_questions = train_matrix_bagged.shape[1]
     model = AutoEncoder(num_questions, k)
     lr = 0.05
     lamb = 0.001
-    train_nn(model, lr, lamb, train_matrix_bagged, zero_train_matrix, valid_data)
+    train_nn(model, lr, lamb, train_matrix_bagged, zero_train_matrix, valid_data, epoch)
     result = evaluate_nn(model, zero_train_matrix, valid_data)
     return result
 
 
-def train_nn(model, lr, lamb, train_data, zero_train_data, valid_data):
+def train_nn(model, lr, lamb, train_data, zero_train_data, valid_data, epoch):
     """ Train the neural network, where the objective also includes
     a regularizer.
 
@@ -95,29 +84,30 @@ def train_nn(model, lr, lamb, train_data, zero_train_data, valid_data):
     :return: None
     """
     model.train()
-    # Define optimizers and loss function.
+
     optimizer = optim.SGD(model.parameters(), lr=lr)
     num_student = train_data.shape[0]
-    train_loss = 0.
 
-    for user_id in range(num_student):
-        inputs = Variable(zero_train_data[user_id]).unsqueeze(0)
-        target = inputs.clone()
+    for epoch in range(epoch):
+        train_loss = 0.
 
-        optimizer.zero_grad()
-        output = model(inputs)
+        for user_id in range(num_student):
+            inputs = Variable(zero_train_data[user_id]).unsqueeze(0)
+            target = inputs.clone()
 
-        # Mask the target to only compute the gradient of valid entries.
-        nan_mask = np.isnan(train_data[user_id].unsqueeze(0).numpy())
-        target[0][nan_mask] = output[0][nan_mask]
+            optimizer.zero_grad()
+            output = model(inputs)
 
-        loss = torch.sum((output - target) ** 2.) + (lamb/2)*model.get_weight_norm() # added regularizer
-        loss.backward()
+            nan_mask = np.isnan(train_data[user_id].unsqueeze(0).numpy())
+            target[0][nan_mask] = output[0][nan_mask]
+            loss = torch.sum((output - target) ** 2.) + \
+                   (lamb/2)*model.get_weight_norm()
+            loss.backward()
 
-        train_loss += loss.item()
-        optimizer.step()
+            train_loss += loss.item()
+            optimizer.step()
 
-    return evaluate_nn(model, zero_train_data, valid_data)
+        return evaluate_nn(model, zero_train_data, valid_data)
 
 
 def evaluate_nn(model, train_data, valid_data):
@@ -149,8 +139,6 @@ def evaluate_ensemble(data, prediction):
     total_prediction = 0
     total_accurate = 0
     for i in range(len(data["is_correct"])):
-        cur_user_id = data["user_id"][i]
-        cur_question_id = data["question_id"][i]
         if prediction[i] and data["is_correct"][i]:
             total_accurate += 1
         if prediction[i] < 0.5 and not data["is_correct"][i]:
@@ -162,23 +150,29 @@ def evaluate_ensemble(data, prediction):
 if __name__ == "__main__":
     train_matrix, valid_data, test_data = load_data()
 
-    train_data_bagged1, train_matrix_bagged1 = bagging()
-    result_knn = eval_knn_base_models(train_data_bagged1, train_matrix_bagged1, valid_data)
+    train_matrix_bagged1 = bagging(train_matrix)
+    result_knn = eval_knn_base_models(11, train_matrix_bagged1, valid_data)
     print(f"KNN accuracy: {evaluate_ensemble(valid_data, result_knn)}")
 
-    train_data_bagged2, train_matrix_bagged2 = bagging()
-    result_nn1 = eval_neural_net_base_model(train_data_bagged2, train_matrix_bagged2, valid_data)
+    train_matrix_bagged2 = bagging(train_matrix)
+    result_knn2 = eval_knn_base_models(16, train_matrix_bagged2, valid_data)
+    print(f"KNN2 accuracy: {evaluate_ensemble(valid_data, result_knn2)}")
+
+    train_matrix_bagged3 = bagging(train_matrix)
+    result_knn3 = eval_knn_base_models(6, train_matrix_bagged3, valid_data)
+    print(f"KNN3 accuracy: {evaluate_ensemble(valid_data, result_knn3)}")
+
+    train_matrix_bagged4 = bagging(train_matrix)
+    result_nn1 = eval_neural_net_base_model(train_matrix_bagged4, valid_data, 17)
     print(f"Neural Net 1 accuracy: {evaluate_ensemble(valid_data, result_nn1)}")
 
-    train_data_bagged3, train_matrix_bagged3 = bagging()
-    result_nn2 = eval_neural_net_base_model(train_data_bagged3, train_matrix_bagged3, valid_data)
+    train_matrix_bagged5 = bagging(train_matrix)
+    result_nn2 = eval_neural_net_base_model(train_matrix_bagged5, valid_data, 18)
     print(f"Neural Net 2 accuracy: {evaluate_ensemble(valid_data, result_nn2)}")
 
-    ensemble_predictions = np.asmatrix([result_knn, result_nn1, result_nn2])
+    ensemble_predictions = np.asmatrix([result_knn, result_nn2, result_nn1])
     average_predictions = np.asarray(ensemble_predictions.mean(axis=0))[0]
-    accuracy = evaluate_ensemble(valid_data, average_predictions)
+    accuracy = evaluate_ensemble(test_data, average_predictions)
     print(accuracy)
 
-    # print(result_nn)
-    # print(result_knn)
 
